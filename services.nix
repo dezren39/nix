8b8033d3b6b0e910a441e4f9e18fdb6239772667
Drewry Pope <drewry.pope@vertexinc.com>
2024-12-12 14:20:18 -0600

    MGM9JJ4V3R   63   2024-12-12 14:20:12   (current)

--- services.nix
+++ services.nix
@@ -1,9 +1,22 @@
 _: {
     services = {
       nix-daemon.enable = true;
-      jankyborders.enable = true;
-      aerospace.enable = true;
-      sketchybar.enable = true;
+      jankyborders = {
+        enable = true;
+        hidpi = true;
+        width = 1.0;
+        style = "square";
+        order = "above";
+      };
+      # aerospace.enable = true; # TODO: switch to this
+      sketchybar = {
+        enable = true;
+        config = ''
+          sketchybar --bar height=24
+          sketchybar --update
+          echo "sketchybar configuration loaded.."
+        '';
+      };
       # yabai.enable = true;
       # chunkwm.enable = true;
       # autossh
